2ff55cc25fe1295020f5d7c802cdaf436d8084ac
LukeFinch <luke100finch@gmail.com>
2025-12-01 20:09:15 -0600

    continued working on authentication ui

---
+++ ai-interview-prep/src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card"
+
+export const SignInView = () => {
+    return (
+        <div className="flex flex-col gap-6">
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <form>Col1</form>
+                    <div className="bg-radial from-gray-600 to-gray-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                        <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
+                        <p className="text-2xl font-semibold text-white">
+                            Meet.AI
+                        </p>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
