838642eb7f726ed996e92ceea89919e15e30cb7b
LukeFinch <luke100finch@gmail.com>
2025-12-03 18:17:09 -0600

    added google and github sign on

--- ai-interview-prep/src/lib/auth.ts
+++ ai-interview-prep/src/lib/auth.ts
@@ -13,4 +13,14 @@ export const auth = betterAuth({
             ...schema,
         }
     }),
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        },
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }
+    }
 });
