87ce67eddeee404ef1930923adc1dc60c49513f1
LukeFinch <luke100finch@gmail.com>
2025-12-03 21:11:53 -0600

    working on page protection

--- ai-interview-prep/src/app/page.tsx
+++ ai-interview-prep/src/app/page.tsx
@@ -1,70 +1,18 @@
-"use client";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/views/home-view"
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client"; //import the auth client
+const Page = async() => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
 
-export default function Home() {
-  const { data: session } = authClient.useSession();
-
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  
-  const signUp = () => {
-    authClient.signUp.email({
-      name,
-      email,
-      password
-    }, {
-      onError: () => {
-        window.alert("Something went wrong");
-      },
-      onSuccess: () => {
-        window.alert("Success!");
-      }
-    });
-  };
-
-  const signIn = () => {
-    authClient.signIn.email({
-      email,
-      password
-    }, {
-      onError: () => {
-        window.alert("Something went wrong");
-      },
-      onSuccess: () => {
-        window.alert("Success!");
-      }
-    });
-  };
-
-  if (session) {
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={() => authClient.signOut()}>
-          Sign out
-        </Button>
-      </div>
-    )
+  if (!session) {
+    redirect("/sign-in");
   }
-  
-  return (
-    <div className="flex flex-col gap-y-10">
-      <div className="p-4 flex flex-col gap-y-4">
-        <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
-        <Input placeholder="email"  value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <Input placeholder="password" type="password"  value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <Button onClick={signUp}>
-          Create user
-        </Button>
-      </div>
-        <Button onClick={signIn}>
-          Sign In
-        </Button>
-    </div>
-  )
+
+  return <HomeView/>
 }
+ 
+export default Page;
---
+++ ai-interview-prep/src/modules/home/views/home-view.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client"; //import the auth client
+
+export const HomeView = () => {
+  const { data: session } = authClient.useSession();
+
+  if(!session) {
+    return (
+      <p>loading...</p>
+    )
+  }
+  
+  return (
+    <div className="flex flex-col p-4 gap-y-4">
+      <p>Logged in as {session.user.name}</p>
+      <Button onClick = {() => authClient.signOut()}>
+        Sign out
+      </Button>
+    </div>
+  )
+}
+
