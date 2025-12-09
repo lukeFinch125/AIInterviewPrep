e5d34b25d97f5b371f7a5e158a19eadaa7383113
LukeFinch <luke100finch@gmail.com>
2025-12-08 18:46:56 -0600

    finished navbar command style and fixed mobile responsiveness, need to add query for data

--- ai-interview-prep/src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ ai-interview-prep/src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -13,11 +13,22 @@ import { AvatarImage } from "@radix-ui/react-avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 const DashboardUserButton = () => {
 
     const { data, isPending } = authClient.useSession();
     const router = useRouter();
+    const isMobile = useIsMobile();
 
     const onLogout = async() => {
     await authClient.signOut({
@@ -31,6 +42,57 @@ const DashboardUserButton = () => {
 
     if (isPending || !data?.user) {
         return null;
+    }
+
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+                    {data.user.image ? (
+                        <Avatar>
+                            <AvatarImage src={data.user.image}/>
+                        </Avatar>
+                    ) : (
+                        <GeneratedAvatar
+                            seed={data.user.name}
+                            variant="initials"
+                            className="size-9 mr-3"
+                        />
+                    )}
+                    <div className="flex flex-col gap-0.5 pl-2 text-left overflow-hidden flex-1 min-w-1 text-white">
+                        <p className="text-sm truncate w-full">
+                            {data.user.name}
+                        </p>
+                        <p className="text-xs truncate w-full">
+                            {data.user.email}
+                        </p>
+                    </div>
+                    <ChevronDownIcon className="size-4 shrink-0 text-white"/>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{data.user.name}</DrawerTitle>
+                        <DrawerDescription>{data.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button
+                            variant="outline"
+                            onClick={() => {}}
+                        >
+                            <p>Billing</p>
+                            <CreditCardIcon className="size-4"/>
+                        </Button>
+                        <Button
+                            variant="outline"
+                            onClick={ onLogout }
+                        >
+                            <p>Logout</p>
+                            <LogOutIcon className="size-4"/>
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        )
     }
 
     return (
